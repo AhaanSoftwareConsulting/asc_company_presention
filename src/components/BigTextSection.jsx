d84a10ff7eaf7d250de39b4834517ad50947f181
Sugam-Dev-25 <sugamkarmakar25@gmail.com>
2026-09-14 19:52:54 +0530

    add section

---
+++ src/components/BigTextSection.jsx
@@ -0,0 +1,26 @@
+const BigTextSection = () => {
+  return (
+    <section className="relative overflow-hidden bg-[#F7F7F5] py-16 select-none">
+      <div className="mx-auto flex max-w-7xl justify-center px-4">
+        <h1
+          className="
+            text-center
+            whitespace-nowrap
+            text-4xl font-black uppercase tracking-widest
+            sm:text-6xl
+            md:text-7xl
+            lg:text-[110px]
+            xl:text-[130px]
+            bg-gradient-to-b from-[#111111]/70 via-[#111111]/20 to-transparent
+            bg-clip-text text-transparent
+            leading-none
+          "
+        >
+          Ahaan Software
+        </h1>
+      </div>
+    </section>
+  );
+};
+
+export default BigTextSection;
